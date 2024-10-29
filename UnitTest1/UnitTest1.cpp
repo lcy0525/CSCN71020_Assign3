6d1bc92f2ec6e8694a3806563ab316c55f278b05
Lcy <lcyyaoyuan@gmail.com>
2024-10-28 23:31:19 -0400

    Assignment2

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -44,14 +44,14 @@ namespace UnitTest1
 			char Player1[10] = "Scissors";
 			char Player2[10] = "Rock";
 			const char* Result = game(Player1, Player2);
-			Assert::AreEqual("Player1", Result);
+			Assert::AreEqual("Player2", Result);
 		}
 		TEST_METHOD(Scissors)
 		{
 			char Player1[10] = "Paper";
 			char Player2[10] = "Scissors";
 			const char* Result = game(Player1, Player2);
-			Assert::AreEqual("Player1", Result);
+			Assert::AreEqual("Player2", Result);
 		}
 
 		TEST_METHOD(Paper)
@@ -59,7 +59,7 @@ namespace UnitTest1
 			char Player1[10] = "Rock";
 			char Player2[10] = "Paper";
 			const char* Result = game(Player1, Player2);
-			Assert::AreEqual("Player1", Result);
+			Assert::AreEqual("Player2", Result);
 		}
 	};
 
